67772eb8085f50613ed234e4b88ad32a33435f54
Abl <fotedev15@gmail.com>
2022-10-13 20:18:24 +0600

    asdasd123

---
+++ src/views/Cart.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="main__content">
+    <div class="container">
+      <div class="row">
+        <p class="cart__wrap"><span class="cart__btn">вернуться назад</span></p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+};
+</script>
+<style scoped>
+.main__content {
+  background-image: url("@/assets/img/texture.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.cart__btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
